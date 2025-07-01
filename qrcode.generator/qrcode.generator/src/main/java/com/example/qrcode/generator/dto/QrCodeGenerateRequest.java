bb42995013b1435b8edf4a0328968fb5663480d3
Vinicius <viniciuslnogi@gmail.com>
2025-06-30 23:28:52 -0300

    feat: gerador de QR Code com integração ao S3

---
+++ qrcode.generator/qrcode.generator/src/main/java/com/example/qrcode/generator/dto/QrCodeGenerateRequest.java
@@ -0,0 +1,5 @@
+package com.example.qrcode.generator.dto;
+
+public record QrCodeGenerateRequest(String text) {
+
+}
---
+++ qrcode.generator/qrcode.generator/src/main/java/com/example/qrcode/generator/dto/QrCodeGenerateResponse.java
@@ -0,0 +1,5 @@
+package com.example.qrcode.generator.dto;
+
+public record QrCodeGenerateResponse(String url) {
+
+}
